fda268f3bbd6848e64bd504502fb862cdd2338e0
jtreeves <jackson.t.reeves@gmail.com>
2020-12-09 10:36:56 -0500

    [App] Add decrement function

--- src/App.js
+++ src/App.js
@@ -15,11 +15,18 @@ class App extends Component {
         this.setState({ count: newCount })
     }
 
+    decrement = () => {
+        const currentCount = this.state.count
+        const newCount = currentCount - 1
+        this.setState({ count: newCount })
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.state.count}</h1>
                 <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
             </div>
         )
     }
