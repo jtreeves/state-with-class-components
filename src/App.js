b9d313443d54c27bd6f02b23f75ff0a5a18c75a2
jtreeves <jackson.t.reeves@gmail.com>
2020-12-09 09:59:59 -0500

    [App] Add constructor

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import './App.css'
 import { Component } from 'react'
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+    }
     render() {
         return (
             <div>
-                App!
+                <h1>NUMBER WILL GO HERE</h1>
+                <button>+</button>
             </div>
         )
     }
