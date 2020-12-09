fa86e390868d27cef2ac7aa2069aa6231c74cd25
jtreeves <jackson.t.reeves@gmail.com>
2020-12-09 10:45:36 -0500

    [App] Add double button

--- src/App.js
+++ src/App.js
@@ -21,12 +21,19 @@ class App extends Component {
         this.setState({ count: newCount })
     }
 
+    double = () => {
+        const currentCount = this.state.count
+        const newCount = currentCount * 2
+        this.setState({ count: newCount })
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.state.count}</h1>
                 <button onClick={this.increment}>+</button>
                 <button onClick={this.decrement}>-</button>
+                <button onClick={this.double}>*</button>
             </div>
         )
     }
