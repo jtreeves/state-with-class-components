301877f81b48ac506e3e65a1e26b2caae64632ef
jtreeves <jackson.t.reeves@gmail.com>
2020-12-09 10:04:17 -0500

    [App] Change number boilerplate to this.state.count to display 0

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>NUMBER WILL GO HERE</h1>
+                <h1>{this.state.count}</h1>
                 <button>+</button>
             </div>
         )
